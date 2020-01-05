a2c1963c3088166ad6bfde09c8606f40edb1b375
tacyuuhon <tacyuuhon@gmail.com>
2020-01-05 18:12:52 +0800

    Update brew

--- tools/tools.lua
+++ tools/tools.lua
@@ -15,5 +15,25 @@ tools:setMenu(
                 os.execute("open https://dict.hjenglish.com/")
             end 
         },
+        {
+            title = "Brew更新",
+            fn = function ()
+
+                hs.task.new("/usr/local/bin/brew", function (code, stdout, stderr)
+                    hs.notify.new({title="brew update", informativeText=stdout}):send()
+
+                    hs.task.new("/usr/local/bin/brew", function (code, stdout, stderr)
+                        hs.notify.new({title="brew upgrade", informativeText=stdout}):send()
+
+                        hs.task.new("/usr/local/bin/brew", function (code, stdout, stderr)
+                            hs.notify.new({title="brew cask upgrade", informativeText=stdout}):send()
+        
+                        end,{"cask", "upgrade"}):start()
+                    end,{"upgrade"}):start()
+
+                end,{"update"}):start()
+   
+            end
+        },
     }
 )
